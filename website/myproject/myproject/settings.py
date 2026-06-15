66415cbeb14cdf69492d6878501e5d84ac05c745
suryadatta32-lgtm <suryadatta32@gmail.com>
2026-06-15 19:10:11 +0530

    push

--- website/myproject/myproject/settings.py
+++ website/myproject/myproject/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
     BASE_DIR / "myapp/static",
 ]
